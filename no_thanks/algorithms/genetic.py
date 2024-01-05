7f45058d0f6c294fd18fb772fb84cc21b79adc3b
Markus Schepke <markus.schepke@wolt.com>
2024-01-05 19:44:43 +0200

    added GeneticTrainer.reset()

--- no_thanks/algorithms/genetic.py
+++ no_thanks/algorithms/genetic.py
@@ -1,5 +1,6 @@
 """Train a strategy using genetic algorithms."""
 
+from typing import Optional, Tuple
 from ..players import ParametricHeuristic
 
 
@@ -14,16 +15,25 @@ class GeneticTrainer:
         mutation_rate: float = 0.1,
     ):
         """Initialize the trainer."""
+
         self.population_size = population_size
         self.generations = generations
         self.inheritance_rate = inheritance_rate
         self.mutation_rate = mutation_rate
 
+        self.current_generation = 0
+        self.current_population_count = 0
+        self.population: Optional[Tuple[ParametricHeuristic, ...]] = None
+
+    def reset(self, mean: float = 0.0, std: float = 1.0) -> None:
+        """Reset the trainer."""
+        self.current_generation = 0
         self.population = tuple(
             ParametricHeuristic.random_weights(
-                name=f"AI #{i:05d}",
-                mean=0.0,
-                std=10.0,
+                name=f"AI #{i:05d} (Gen #00000)",
+                mean=mean,
+                std=std,
             )
             for i in range(self.population_size)
         )
+        self.current_population_count = self.population_size
