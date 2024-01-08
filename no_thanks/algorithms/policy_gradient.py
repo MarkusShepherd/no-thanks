1a2b83943db1bc4fec7179c6731ff699919610bf
Markus Schepke <markus.schepke@wolt.com>
2024-01-08 08:49:18 +0200

    first version of PolicyGradientTrainer

--- no_thanks/algorithms/policy_gradient.py
+++ no_thanks/algorithms/policy_gradient.py
@@ -1,22 +1,25 @@
 """Policy gradient algorithm."""
 
-from typing import Any, List, Optional
+import dataclasses
+from pathlib import Path
+import random
+from typing import Any, List, Optional, Tuple, Union
 import numpy as np
 
 import torch
 from torch import nn
 from torch import optim
 
-from no_thanks.core import Action, Game
+from no_thanks.core import Action, Game, GameState
 from no_thanks.players import HeuristicPlayer
 
 
 class PolicyNetwork(nn.Module):
     """Policy network."""
 
-    def __init__(self, input_size, output_size):
+    def __init__(self):
         super().__init__()
-        self.fc = nn.Linear(input_size, output_size)
+        self.fc = nn.Linear(len(dataclasses.fields(GameState)), 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -102,3 +105,73 @@ class PolicyGradientPlayer(HeuristicPlayer):
 
         policy_loss.backward()
         self.optimizer.step()
+
+
+class PolicyGradientTrainer:
+    """Train a policy gradient player."""
+
+    current_game_num: int
+    players: Tuple[PolicyGradientPlayer, ...]
+
+    def __init__(self, num_games: int = 1_000):
+        self.num_games = num_games
+
+    def reset(self) -> None:
+        """Reset the trainer."""
+        self.current_game_num = 0
+        self.players = tuple(
+            PolicyGradientPlayer(
+                name=f"PG #{i:03d}",
+                policy_net=PolicyNetwork(),
+            )
+            for i in range(Game.NUM_PLAYERS_MAX)
+        )
+
+    def resume(self, save_dir: Union[str, Path]) -> None:
+        """Resume training."""
+        raise NotImplementedError
+
+    def play_game(
+        self,
+        *,
+        min_players: Optional[int] = None,
+        max_players: Optional[int] = None,
+    ) -> Game:
+        """Play a game."""
+
+        print(f"Playing game {self.current_game_num + 1} of {self.num_games}…")
+
+        min_players = min_players or Game.NUM_PLAYERS_MIN
+        max_players = max_players or Game.NUM_PLAYERS_MAX
+
+        assert (
+            Game.NUM_PLAYERS_MIN <= min_players <= max_players <= Game.NUM_PLAYERS_MAX
+        )
+
+        num_players = (
+            min_players
+            if min_players == max_players
+            else random.randint(min_players, max_players)
+        )
+        players = random.sample(self.players, num_players)
+
+        game = Game(players=players)
+        game.play()
+        game.update_elo_ratings()
+
+        winning_score = max(player.score for player in players)
+        # TODO: should we increase the penalty for losing by the margin?
+        rewards = [-1 if player.score < winning_score else +1 for player in players]
+
+        for player, reward in zip(self.players, rewards):
+            player.update_weights(reward)
+
+        self.current_game_num += 1
+
+        return game
+
+    def train(self) -> None:
+        """Train policy gradient players."""
+
+        for _ in range(self.num_games):
+            self.play_game()
